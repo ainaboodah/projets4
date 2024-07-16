7b37d768847f6aa53052ab33ac3aef942f0ba44e
yoannah05 <hyyoannah@gmail.com>
2024-07-16 15:33:13 +0300

    ajax

--- application/views/admindashboard.php
+++ application/views/admindashboard.php
@@ -53,14 +53,11 @@
 <script type="text/javascript">
     var options = {
       series: [{
-      name: 'Income',
+      name: 'Payés',
       data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
     }, {
-      name: 'Expense',
+      name: 'Non Payés',
       data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-    }, {
-      name: 'Transfer',
-      data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
     }],
       chart: {
       type: 'bar',
